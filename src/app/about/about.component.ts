8420a6376eaf67528ba9bbc7b20a9127ee13e7fe
umutatras <umutatras@gmail.com>
2026-02-23 00:47:15 +0300

    init

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,14 @@
+import { Component, ViewEncapsulation } from "@angular/core";
+
+@Component({
+  standalone:true,
+  templateUrl:'./about.component.html',
+  styleUrl:'./about.component.css',
+  imports:[],
+  providers:[],
+  encapsulation:ViewEncapsulation.None
+})
+export class AboutComponent{
+
+}
+
