e8d0be246534fbb81b8e8921fee6258da4039323
umutatras <umutatras@gmail.com>
2026-02-24 22:10:19 +0300

    .

---
+++ src/app/example/example.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  imports: [],
+  templateUrl: './example.html',
+  styleUrl: './example.css',
+})
+export class Example {
+
+}
