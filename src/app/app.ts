7b7e508b62cfe14089ca75ea2d9c5522c7f08f66
umutatras <umutatras@gmail.com>
2026-02-24 18:59:47 +0300

    ngclass

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,16 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { Contact } from './contact/contact';
 import { Home } from "./home/home";
 import { AboutComponent } from "./about/about.component";
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [ Contact, Home, AboutComponent],
+  imports: [ Contact, Home, AboutComponent,NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  bgColor="red";
   helloWorld: string = 'Hello World abii';
-
+ pClassName: string = 'red';
   clickMe(){
  alert('Tıklandı');
   }
